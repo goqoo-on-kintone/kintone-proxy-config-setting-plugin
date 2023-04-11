0650a1510fd843818c8512babb850b27df6dd6c5
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-11 12:28:00 +0900

    feat: dev-serverに対応

---
+++ src/loaders/address.js
@@ -0,0 +1,4 @@
+const webfontScript = document.createElement('script')
+webfontScript.src = `https://localhost:5173/src/apps/address/index.ts`
+webfontScript.type = 'module'
+document.body.appendChild(webfontScript)
---
+++ src/loaders/customer.js
@@ -0,0 +1,4 @@
+const webfontScript = document.createElement('script')
+webfontScript.src = `https://localhost:5173/src/apps/customer/index.ts`
+webfontScript.type = 'module'
+document.body.appendChild(webfontScript)
