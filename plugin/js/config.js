92157b8ddfb07a2ef47399676d0e5a024acfbe7e
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-19 08:32:24 +0900

    chore: プラグインのpack, upload環境作成

---
+++ plugin/js/config.js
@@ -0,0 +1,25 @@
+;(function (PLUGIN_ID) {
+  'use strict'
+
+  var $form = document.querySelector('.js-submit-settings')
+  var $cancelButton = document.querySelector('.js-cancel-button')
+  var $message = document.querySelector('.js-text-message')
+  if (!$form || !$cancelButton || !$message) {
+    throw new Error('Required elements do not exist.')
+  }
+  var config = kintone.plugin.app.getConfig(PLUGIN_ID)
+
+  if (config.message) {
+    $message.value = config.message
+  }
+  $form.addEventListener('submit', function (e) {
+    e.preventDefault()
+    kintone.plugin.app.setConfig({ message: $message.value }, function () {
+      alert('The plug-in settings have been saved. Please update the app!')
+      window.location.href = '../../flow?app=' + kintone.app.getId()
+    })
+  })
+  $cancelButton.addEventListener('click', function () {
+    window.location.href = '../../' + kintone.app.getId() + '/plugin/'
+  })
+})(kintone.$PLUGIN_ID)
---
+++ plugin/js/desktop.js
@@ -0,0 +1,24 @@
+;(function (PLUGIN_ID) {
+  'use strict'
+
+  kintone.events.on('app.record.index.show', function () {
+    var config = kintone.plugin.app.getConfig(PLUGIN_ID)
+
+    var spaceElement = kintone.app.getHeaderSpaceElement()
+    if (spaceElement === null) {
+      throw new Error('The header element is unavailable on this page')
+    }
+    var fragment = document.createDocumentFragment()
+    var headingEl = document.createElement('h3')
+    var messageEl = document.createElement('p')
+
+    messageEl.classList.add('plugin-space-message')
+    messageEl.textContent = config.message
+    headingEl.classList.add('plugin-space-heading')
+    headingEl.textContent = 'Hello kintone plugin!'
+
+    fragment.appendChild(headingEl)
+    fragment.appendChild(messageEl)
+    spaceElement.appendChild(fragment)
+  })
+})(kintone.$PLUGIN_ID)
