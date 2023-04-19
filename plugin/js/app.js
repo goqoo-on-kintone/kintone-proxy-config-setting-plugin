e6b9fccde1855b445599fdbd2a092b223ebe7615
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-19 22:09:09 +0900

    chore: mobileとdesktopのコードは統合

--- plugin/js/app.js
+++ plugin/js/app.js
@@ -1,10 +1,10 @@
 ;(function (PLUGIN_ID) {
   'use strict'
 
-  kintone.events.on('app.record.index.show', function () {
+  kintone.events.on(['app.record.index.show', 'mobile.app.record.index.show'], function () {
     var config = kintone.plugin.app.getConfig(PLUGIN_ID)
 
-    var spaceElement = kintone.app.getHeaderSpaceElement()
+    var spaceElement = kintone.app.getHeaderSpaceElement() ?? kintone.mobile.app.getHeaderSpaceElement()
     if (spaceElement === null) {
       throw new Error('The header element is unavailable on this page')
     }
