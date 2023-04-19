2c06d55daad3850dbf4943fe45574ab739edfcc9
Hisaki Akaza <4494300+the-red@users.noreply.github.com>
2023-04-20 01:42:34 +0900

    feat: kintone-ui-componentのGuidesページのサンプルをTypeScriptで実装

--- src/apps/app/main.ts
+++ src/apps/app/main.ts
@@ -1,22 +1,25 @@
-;(function (PLUGIN_ID) {
-  kintone.events.on(['app.record.index.show', 'mobile.app.record.index.show'], () => {
-    const config = kintone.plugin.app.getConfig(PLUGIN_ID)
-
-    const spaceElement = kintone.app.getHeaderSpaceElement() ?? kintone.mobile.app.getHeaderSpaceElement()
-    if (spaceElement === null) {
-      throw new Error('The header element is unavailable on this page')
+import { DateTime } from 'luxon'
+;((PLUGIN_ID) => {
+  kintone.events.on(['app.record.detail.show', 'mobile.app.record.detail.show'], (event) => {
+    const CONFIG = kintone.plugin.app.getConfig(PLUGIN_ID)
+    // Get today's date with yyyy-mm-dd
+    const currentDate = DateTime.local().toISODate()!
+    const referenceDate = CONFIG.date
+    const condition = CONFIG.condition
+    const targetFields = JSON.parse(CONFIG.targetFields) as string[]
+    const fieldColor = '#e74c3c'
+    let isBefore = false
+    if (currentDate < referenceDate) {
+      isBefore = true
     }
-    const fragment = document.createDocumentFragment()
-    const headingEl = document.createElement('h3')
-    const messageEl = document.createElement('p')
-
-    messageEl.classList.add('plugin-space-message')
-    messageEl.textContent = config.message
-    headingEl.classList.add('plugin-space-heading')
-    headingEl.textContent = 'Hello kintone plugin!'
-
-    fragment.appendChild(headingEl)
-    fragment.appendChild(messageEl)
-    spaceElement.appendChild(fragment)
+    // Change the fields' color to red depending on the conditions set in the plug-in
+    if ((isBefore && condition === 'before') || (!isBefore && condition === 'after')) {
+      targetFields.forEach((targetField) => {
+        const fieldElement =
+          kintone.app.record.getFieldElement(targetField) ?? kintone.mobile.app.record.getFieldElement(targetField)
+        if (!fieldElement) return
+        fieldElement.style.backgroundColor = fieldColor
+      })
+    }
   })
 })(kintone.$PLUGIN_ID)
--- src/apps/config/main.ts
+++ src/apps/config/main.ts
@@ -1,23 +1,169 @@
-;(function (PLUGIN_ID) {
-  const $form = document.querySelector<HTMLFormElement>('.js-submit-settings')
-  const $cancelButton = document.querySelector<HTMLButtonElement>('.js-cancel-button')
-  const $message = document.querySelector<HTMLInputElement>('.js-text-message')
-  if (!$form || !$cancelButton || !$message) {
-    throw new Error('Required elements do not exist.')
-  }
-  const config = kintone.plugin.app.getConfig(PLUGIN_ID)
-
-  if (config.message) {
-    $message.value = config.message
-  }
-  $form.addEventListener('submit', function (e) {
-    e.preventDefault()
-    kintone.plugin.app.setConfig({ message: $message.value }, function () {
-      alert('The plug-in settings have been saved. Please update the app!')
-      window.location.href = '../../flow?app=' + kintone.app.getId()
-    })
+import { Button, DatePicker, Dialog, Dropdown, MultiChoice } from 'kintone-ui-component'
+;(async (PLUGIN_ID) => {
+  // Create DatePicker component
+  const datePicker = new DatePicker({
+    label: 'Reference Date',
+    requiredIcon: true,
+    language: 'auto',
   })
-  $cancelButton.addEventListener('click', function () {
-    window.location.href = '../../' + kintone.app.getId() + '/plugin/'
+  // Create Dropdown component
+  const dropdown = new Dropdown({
+    label: 'Condition',
+    requiredIcon: true,
+    items: [
+      {
+        label: '-----',
+        value: '-----',
+      },
+      {
+        label: 'Before reference date',
+        value: 'before',
+      },
+      {
+        label: 'After reference date',
+        value: 'after',
+      },
+    ],
+    value: '-----',
+  })
+  // Get field info to display in MultiChoice component
+  const items = await getFields()
+  // Create MultiChoice component
+  const multiChoice = new MultiChoice({
+    label: 'Fields',
+    requiredIcon: true,
+    items: items,
+  })
+  // Create Button component
+  const saveButton = new Button({
+    text: 'Save',
+    type: 'submit',
+  })
+  const cancelButton = new Button({
+    text: 'Cancel',
+    type: 'normal',
+    id: 'kuc_cancel_button',
+  })
+  const dialogOKButton = new Button({
+    text: 'OK',
+    type: 'submit',
+  })
+  const dialogCancelButton = new Button({
+    text: 'Cancel',
+    type: 'normal',
+    id: 'kuc_dialog_cancel_button',
+  })
+  const divEl = document.createElement('div')
+  divEl.setAttribute('id', 'kuc_dialog_footer')
+  // Create Dialog component
+  const dialog = new Dialog({
+    content: `<div style="text-align: center; padding: 48px 24px">
+              <p style="margin: 0;">The target fields are colored according to the conditions.</p>
+              <p>Are you sure to save your settings?</p>
+              <div>`,
+    footer: divEl,
+  })
+
+  // Get field info of tha app
+  function getFields() {
+    const param = { app: kintone.app.getId() }
+    return kintone.api(kintone.api.url('/k/v1/preview/app/form/fields', true), 'GET', param).then((resp) => {
+      const items = []
+      for (const key in resp.properties) {
+        // eslint-disable-next-line
+        if (!resp.properties.hasOwnProperty(key)) {
+          continue
+        }
+        const prop = resp.properties[key]
+        const label = prop.label
+        const code = prop.code
+        switch (prop.type) {
+          case 'SINGLE_LINE_TEXT':
+          case 'NUMBER':
+          case 'CALC':
+          case 'RADIO_BUTTON':
+          case 'DROP_DOWN':
+          case 'RECORD_NUMBER':
+          case 'MULTI_LINE_TEXT':
+          case 'CHECK_BOX':
+          case 'MULTI_SELECT':
+          case 'DATE':
+          case 'DATETIME':
+          case 'CREATED_TIME':
+          case 'UPDATED_TIME':
+            items.push({ label: label, value: code })
+            break
+          default:
+            break
+        }
+      }
+      return items
+    })
+  }
+
+  // Display the value when saved last time
+  const config = kintone.plugin.app.getConfig(PLUGIN_ID)
+  if (Object.keys(config).length) {
+    datePicker.value = config.date
+    dropdown.value = config.condition
+    multiChoice.value = JSON.parse(config.targetFields)
+  }
+
+  // Display components
+  const dateSpaceEl = document.getElementById('date_space')!
+  dateSpaceEl.appendChild(datePicker)
+  const dropdownSpaceEl = document.getElementById('dropdown_space')!
+  dropdownSpaceEl.appendChild(dropdown)
+  const multiChoiceSpaceEl = document.getElementById('multichoice_space')!
+  multiChoiceSpaceEl.appendChild(multiChoice)
+  const buttonSpaceEl = document.getElementById('button_space')!
+  buttonSpaceEl.appendChild(cancelButton)
+  buttonSpaceEl.appendChild(saveButton)
+
+  // When the Save button is clicked
+  saveButton.addEventListener('click', (event) => {
+    // Reset error messages
+    datePicker.error = ''
+    dropdown.error = ''
+    multiChoice.error = ''
+    // Check required itmes
+    let requiredFlag = false
+    if (!datePicker.value) {
+      datePicker.error = 'Please enter'
+      requiredFlag = true
+    }
+    if (dropdown.value === '-----') {
+      dropdown.error = 'Please select'
+      requiredFlag = true
+    }
+    if (!multiChoice.value.length) {
+      multiChoice.error = 'Please select'
+      requiredFlag = true
+    }
+    if (requiredFlag) return
+    dialog.open()
+    divEl.appendChild(dialogCancelButton)
+    divEl.appendChild(dialogOKButton)
+  })
+  // When the Cancel button is clicked
+  cancelButton.addEventListener('click', (event) => {
+    history.back()
+  })
+
+  // When the OK button in Dialog is clicked
+  dialogOKButton.addEventListener('click', (event) => {
+    const selectedFields = JSON.stringify(multiChoice.value)
+    const date = datePicker.value
+    const condition = dropdown.value
+    const config = {
+      date: date,
+      condition: condition,
+      targetFields: selectedFields,
+    }
+    kintone.plugin.app.setConfig(config)
+  })
+  // When the Cancel button in Dialog is clicked
+  dialogCancelButton.addEventListener('click', (event) => {
+    dialog.close()
   })
 })(kintone.$PLUGIN_ID)
